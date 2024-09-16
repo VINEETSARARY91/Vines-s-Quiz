d655ff90122f712ecf8d789009690fcce6870a00
Vineet sarary <vs9224966@gmail.com>
2024-09-16 16:51:10 +0530

    Home.js added

---
+++ home.js
@@ -0,0 +1,22 @@
+const h4tag = document.querySelectorAll('h4')
+const start = document.querySelector('.start')
+let opacitylevel = 0.1;
+let intervalId;
+
+function upadeteh4() {
+   
+   opacitylevel= opacitylevel + .1
+   h4tag.forEach((e, ind) => {
+   e.style.opacity = `${opacitylevel}`
+   if(opacitylevel>=1){
+   clearInterval(intervalId)
+   console.log("clearInterval")
+}
+
+});
+};
+intervalId = setInterval(upadeteh4,100)
+
+start.addEventListener('click', function (){
+   window.location.href = "index.html"
+});
